d02a0a84c415ee4e4fab1713b05eb48113f52446
Zhonghe Zheng <zhengzh315@gmail.com>
2023-06-15 18:38:10 +1000

    create prompt form and API route

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,4 +1,60 @@
-const Form = () => {
-  return <div>Form</div>;
+import Link from "next/link";
+
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        {" "}
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share prompts with the world, and let others leave a comment!
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder="Write your prompt here..."
+            required
+            className="form_textarea"
+          />
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag{" "}
+            <span className="font-normal">
+              (#product,#webdevelopment etc...)
+            </span>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            placeholder="#tag"
+            required
+            className="form_input"
+          />
+        </label>
+        <div className="flex-end mx-3 mb-5 gap-4">
+          <Link href="/">Cancel</Link>
+          <button
+            type="submit"
+            disabled={submitting}
+            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
+
 export default Form;
