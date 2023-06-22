2a8f7ccf1d7d242600b5f1b621c5fcf07a4d6d7d
zzh315 <zhengzh315@gmail.com>
2023-06-22 19:37:03 +1000

    fixed google auth mongodb schema error and added github profile

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,10 +3,9 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-import { useRouter } from "next/navigation";
+
 const Nav = () => {
   const { data: session } = useSession();
-  const router = useRouter();
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
