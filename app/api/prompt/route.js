7b20ab089285b1b4d53accd567c84f4ff5baf55e
zzh315 <zhengzh315@gmail.com>
2023-06-22 23:06:06 +1000

    304 fix 8

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,14 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 import { revalidatePath } from "next/cache";
+import User from "@models/user";
 
 export const GET = async (req, res) => {
   try {
-    revalidatePath("/api/prompt");
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-
+    revalidatePath("/api/prompt");
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify("Failed to fetch all prompts"), {
