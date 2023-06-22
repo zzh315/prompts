7986ecba079addccc428b8f85de36f7abc04ce37
zzh315 <zhengzh315@gmail.com>
2023-06-22 20:19:29 +1000

    fix SSR caching 304 not modified error

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,7 +6,10 @@ export const GET = async (req, res) => {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
-
+    res.setHeader(
+      "Cache-Control",
+      "no-cache, no-store, max-age=0, must-revalidate"
+    );
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify("Failed to fetch all prompts"), {
