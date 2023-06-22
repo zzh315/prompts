9250b9376c449ffa7aceda9ac84ded698a72ca7a
zzh315 <zhengzh315@gmail.com>
2023-06-22 20:25:23 +1000

    header fix 2

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -3,13 +3,14 @@ import Prompt from "@models/prompt";
 
 export const GET = async (req, res) => {
   try {
-    await connectToDB();
-
-    const prompts = await Prompt.find({}).populate("creator");
     res.setHeader(
       "Cache-Control",
       "no-cache, no-store, max-age=0, must-revalidate"
     );
+    await connectToDB();
+
+    const prompts = await Prompt.find({}).populate("creator");
+
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify("Failed to fetch all prompts"), {
