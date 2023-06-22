a834ad0047d68c4bdd298448c466e51f1f2ccb60
zzh315 <zhengzh315@gmail.com>
2023-06-22 23:15:55 +1000

    final fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -9,7 +9,17 @@ export const GET = async (req, res) => {
 
     const prompts = await Prompt.find({}).populate("creator");
     revalidatePath("/api/prompt");
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    const response = new Response(JSON.stringify(prompts), { status: 200 });
+    const url = new URL(request.url);
+    url.searchParams.set("t", Date.now());
+    response.headers.set(
+      "Cache-Control",
+      "no-cache, no-store, must-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Location", url.toString());
+    return response;
   } catch (error) {
     return new Response(JSON.stringify("Failed to fetch all prompts"), {
       status: 500,
